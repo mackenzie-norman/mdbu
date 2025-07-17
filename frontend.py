415108f05e418aeb7a1546b9462d47109687724b
mackenzie-norman <115865992+mackenzie-norman@users.noreply.github.com>
2025-07-17 09:01:24 -0700

    okay this is almost ready for testing

--- frontend.py
+++ frontend.py
@@ -24,24 +24,35 @@ config:
 """
 
 
-def selector(x):
+def selector(songs):
     global manager
     write_tracklist = True
     copy_files = False
 
+    old_windows = [w for w in manager]
+
+    window = ptg.Window(fullscreen=True)
+
     def flip_var(x):
         x = not x
+        print(f"Writing {len(songs)} songs to tracklist.txt")
+        with open("tracklist.txt", "w") as f:
+            for l in songs:
+                f.write(l.location.split("/")[-1])
+                f.write("\n")
+        manager.remove(window)
+        for w in old_windows:
+            manager.add(w)
 
-    window = ptg.Window()
-    for l in x:
-        window += l.location
-    window += ["Burn to CD", lambda x: flip_var(copy_files)]
+    song_list = ptg.Container()
+    for l in songs:
+        song_list += l.name
+    layout = ptg.Splitter(song_list, ptg.Container())
+    window += layout
+    window += ptg.Button("Burn to CD", lambda x: flip_var(copy_files))
     manager.add(window)
-    print(f"Writing {len(x)} songs to tracklist.txt")
-    with open("tracklist.txt", "w") as f:
-        for l in x:
-            f.write(l.location.split("/")[-1])
-            f.write("\n")
+    for w in old_windows:
+        manager.remove(w)
 
 
 def list_picker(options):
