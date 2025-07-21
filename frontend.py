c3a198848cf83d6085b522905e1b52afc1dc2909
mackenzie-norman <115865992+mackenzie-norman@users.noreply.github.com>
2025-07-20 17:33:45 -0700

    quick commit before I switch to layouts

--- frontend.py
+++ frontend.py
@@ -24,53 +24,60 @@ config:
 """
 
 
-def selector(songs):
-    global manager
-    write_tracklist = True
-    copy_files = False
-
-    old_windows = [w for w in manager]
-
-    window = ptg.Window(fullscreen=True)
-
-    def flip_var(x):
-        x = not x
-        print(f"Writing {len(songs)} songs to tracklist.txt")
-        with open("tracklist.txt", "w") as f:
-            for l in songs:
-                f.write(l.location.split("/")[-1])
-                f.write("\n")
-        #copy_tracklist([s.name for s in songs])
-        copy_tracklist()
-        manager.remove(window)
-        for w in old_windows:
-            manager.add(w)
-
-
-    song_list = ptg.Container()
-    for l in songs:
-        song_list += l.name
-    layout = ptg.Splitter(song_list, ptg.Container())
-    window += layout
-    window += ptg.Button("Burn to CD", lambda x: flip_var(copy_files))
-    manager.add(window)
-    for w in old_windows:
-        manager.remove(w)
-
-
-def list_picker(options):
-    return_win = ptg.Window()
-    splitter = ptg.Splitter()
-    for l in list(options.keys()):
-        return_win += [str(l), lambda x: selector(options[x.label])]
-    # return_win += splitter
-    return return_win
-
-
-with ptg.YamlLoader() as loader:
-    loader.load(CONFIG)
-
 with ptg.WindowManager() as manager:
+
+    def selector(songs, title="Untitled"):
+        global manager
+        write_tracklist = True
+        copy_files = False
+
+        old_windows = [w for w in manager]
+
+        window = ptg.Window().set_title(f"[210 bold] {title}").center()
+
+        def flip_var(x):
+            x = not x
+            print(f"Writing {len(songs)} songs to tracklist.txt")
+            with open("tracklist.txt", "w") as f:
+                for l in songs:
+                    f.write(l.location.split("/")[-1])
+                    f.write("\n")
+            # copy_tracklist([s.name for s in songs])
+            copy_tracklist()
+            manager.remove(window)
+            for w in old_windows:
+                manager.add(w)
+
+        song_list = ptg.Container()
+        for l in songs:
+            splt = ptg.Splitter(l.name, ptg.Checkbox(True)).set_char("seperator", "")
+            song_list += splt
+            # song_list += l.name
+
+        max_char_len = max([len(song.name) for song in songs])
+        window.width = max_char_len * 3
+        layout = ptg.Splitter(song_list, ptg.Container())
+        window += layout
+        window += ptg.Splitter(
+            ptg.Button("Burn to CD", lambda x: flip_var(copy_files)),
+            ptg.Button("Back", show_albums),
+        )
+        clear_windows()
+        manager.add(window)
+
+    def list_picker(options):
+        return_win = ptg.Window()
+        splitter = ptg.Splitter()
+        for l in list(options.keys()):
+            return_win += [str(l), lambda x: selector(options[x.label], x.label)]
+        # return_win += splitter
+        return return_win
+
+    def clear_windows():
+        global manager
+        for w in manager:
+            manager.remove(w)
+
     library = read_library()
     show_albums = lambda x: show_albums(x)
     window = (
@@ -89,11 +96,12 @@ with ptg.WindowManager() as manager:
     )
 
     def show_albums(x):
-        manager.remove(window)
+        global manager
+        clear_windows()
         albums = library.albums()
-        # print(str(albums), file=open("test.txt", "w"))
         manager.add(list_picker(albums))
 
     manager.add(window)
     # manager.add(list_picker(library.artists()))
     # manager
+    manager.__enter__()
